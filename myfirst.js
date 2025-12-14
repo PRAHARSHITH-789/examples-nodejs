b5499d4b3a203ba3f274b22361ac4a200a109c0e
PRAHARSHITH-789 <siddhu.vakkapatla@gmail.com>
2025-12-14 08:35:33 +0530

    this is node,js

--- myfirst.js
+++ myfirst.js
@@ -3,6 +3,6 @@ var http = require('http');
 http
   .createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
+    res.end('Hello World this is node.js project!');
   })
   .listen(8080);
